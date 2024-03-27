26818fe5866cf49915807a3eb6c085ed4c087743
leeyeojin <yjlee@pndtech.co.kr>
2024-03-27 18:59:06 +0900

    main header js

--- js/common.js
+++ js/common.js
@@ -1,6 +1,22 @@
-const header = document.querySelector('header');
-const footer = document.querySelector('footer');
-const index = 'index'; // URL경로에서 매칭되는 텍스트
+
+//main 스크롤 내릴 때 header bg 변경
+
+window.addEventListener('scroll', function() {
+  var header = document.querySelector('.main-header');
+  var scrollPosition = window.scrollY;
+
+  // 스크롤이 일정 이상 내려갔을 때 배경색 변경
+  if (scrollPosition > 36) { // 변경하고 싶은 스크롤 위치 (100은 예시입니다)
+    header.style.backgroundColor = '#fff'; // 변경할 배경색
+  } else {
+    header.style.backgroundColor = '#F4F6F8'; // 초기 배경색
+  }
+});
+
+// const header = document.querySelector('header');
+// const footer = document.querySelector('footer');
+// const index = 'index';
+ // URL경로에서 매칭되는 텍스트
 
 // const IndexLayout = () => {
 //   header.innerHTML = `
@@ -25,10 +41,13 @@ const index = 'index'; // URL경로에서 매칭되는 텍스트
 // }
 
 
-console.log(location.pathname); // URL의 경로 나타냄
+// console.log(location.pathname); 
+// // URL의 경로 나타냄
 
-if(location.pathname.match(index)){ // index변수값과 비교되는 텍스트가 있다면 true
-  IndexLayout();
-}else{
-  SubLaytou();
-} 
+// if(location.pathname.match(index)){ 
+//   // index변수값과 비교되는 텍스트가 있다면 true
+//   IndexLayout();
+// }else{
+//   SubLaytou();
+// } 
+
